1341a5dc5bf3b79ee57d6cb7d1a66a3ea8fc3989
StjepanPrakljacic <stjepanprakljacic@gmail.com>
2024-02-13 23:24:22 +0100

    feature/PEP8-parser: Adding program and test scripts.

---
+++ Test/test_script_1.py
@@ -0,0 +1,29 @@
+"Test doc"
+import sys
+import os, time, math
+"New string"
+def some_function(value):
+    import random
+    return value
+class Example:
+    def method1(self):
+        pass
+    def method2(self):
+        pass
+
+first_name = "Stjepan"
+
+
+
+def another_function():
+    pass
+class AnotherClass:
+
+    last_nema = "Prakljacic"
+
+
+    def some_method(self):
+        pass
+
+import xml, json
+from math import sqrt, pow
